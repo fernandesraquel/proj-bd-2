64433c50f399561d4ba44c5c38e6fec0ef0c3ee3
Raquel Fernandes <raquel.lima.072@ufrn.edu.br>
2023-04-28 09:25:12 -0300

    add inserts

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -2,7 +2,7 @@
 CREATE TABLE funcionario(
   codigo SERIAL PRIMARY KEY,
   nome VARCHAR(200) NOT NULL, 
-  sexo char,
+  sexo CHAR(1),
   dt_nasc DATE, 
   salario DECIMAL(10,2)
 );
@@ -11,10 +11,9 @@ CREATE TABLE funcionario(
 CREATE TABLE departamento(
   codigo SERIAL PRIMARY KEY,
   descricao VARCHAR(500), 
-  cod_gerente int,
+  cod_gerente INT,
  
-  FOREIGN KEY (cod_gerente) REFERENCES funcionario(codigo) 
-                      
+  FOREIGN KEY (cod_gerente) REFERENCES funcionario(codigo)                     
 );
 
 ALTER TABLE funcionario ADD COLUMN cod_dep INT;
@@ -25,14 +24,13 @@ CREATE TABLE projeto (
   codigo SERIAL PRIMARY KEY,
   nome VARCHAR(100) NOT NULL, 
   descricao VARCHAR(500) NOT NULL, 
-  cod_depto int, 
+  cod_depto INT, 
   cod_responsavel INT, 
   data_inicio DATE, 
   data_fim DATE,
   
   FOREIGN KEY(cod_depto) REFERENCES departamento(codigo),
-  FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo)
-  
+  FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo) 
 );
 
 -- Tabela atividade
---
+++ tarefas/t01/tarefa01-inserts.sql
@@ -0,0 +1,46 @@
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_dep) VALUES
+('João da Silva Ribeiro', 'M', '1990-01-01', 5000.00, NULL),
+('Maria de Souza Ferreira', 'F', '1992-05-12', 4500.00, NULL),
+('Pedro Nascimento de Araújo', 'M', '1985-10-20', 6500.00, NULL),
+('Ana Beatriz Fernandes', 'F', '1994-02-15', 4000.00, NULL),
+('Lucas Santos Batista', 'M', '1988-09-18', 5500.00, NULL),
+('Carla Oliveira de Medeiros', 'F', '1991-07-07', 4800.00, NULL),
+('Marcelo Nunes de Brito Junior', 'M', '1982-12-30', 7000.00, NULL),
+('Julia Vieira dos Santos', 'F', '1993-11-25', 4200.00, NULL);
+
+INSERT INTO departamento (descricao, cod_gerente) VALUES
+('Departamento de Vendas', 1),
+('Departamento de Marketing', 2),
+('Departamento Financeiro', 3),
+('Departamento de Recursos Humanos', 4),
+('Depatamento de Tecnologia da Informação', 5);
+
+
+UPDATE funcionario SET cod_dep = 1 WHERE codigo = 1;
+UPDATE funcionario SET cod_dep = 2 WHERE codigo = 2;
+UPDATE funcionario SET cod_dep = 3 WHERE codigo = 3;
+UPDATE funcionario SET cod_dep = 4 WHERE codigo = 4;
+UPDATE funcionario SET cod_dep = 5 WHERE codigo = 5;
+UPDATE funcionario SET cod_dep = 2 WHERE codigo = 6;
+UPDATE funcionario SET cod_dep = 1 WHERE codigo = 7;
+
+INSERT INTO projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) VALUES
+('Lançamento de Novos Produtos', 'Desenvolver novos produtos para o mercado', 2, 4, '2023-05-10', '2023-12-31'),
+('Desenvolvimento de Campanhas Publicitárias', 'Criar campanhas publicitárias para promover os produtos da empresa', 1, 1, '2023-05-10', '2023-12-31'),
+('Redução de Custos', 'Identificar áreas onde os custos podem ser reduzidos sem prejudicar a eficiência da empresa', 4, 3, '2023-05-10', '2023-12-31'),
+('Recrutamento e Seleção', 'Identificar e recrutar os melhores candidatos para preencher as vagas da empresa', 3, 5, '2023-05-10', '2023-12-31'),
+('Implantação de Sistema de Gerenciamento de Projetos', 'Implantar um sistema de gerenciamento de projetos para aumentar a eficiência da empresa', 5, 2, '2023-05-10', '2023-12-31');
+
+INSERT INTO atividade(nome, descricao, cod_responsavel, data_inicio, data_fim) VALUES
+('Análise de mercado e identificação de demandas', 'Desenvolvimento de campanha publicitária para o lançamento dos produtos', 3, '2023-05-10', '2023-05-31'),
+('Definição do público-alvo e identificação de canais de comunicação adequados', 'Acompanhamento e análise de resultados das campanhas', 5, '2023-05-10', '2023-05-31'),
+('Identificação de áreas com potencial para redução de custos', 'Implementação de políticas e procedimentos para reduzir gastos desnecessários', 4, '2023-05-10', '2023-05-31'),
+('Anúncio das vagas e divulgação da empresa para potenciais candidatos', 'Triagem de currículos e realização de entrevistas', 2, '2023-05-10', '2023-05-31'),
+('Identificação das necessidades e requisitos para o sistema de gerenciamento de projetos', 'Avaliação e ajustes contínuos no sistema para garantir sua efetividade', 1, '2023-05-10', '2023-05-31');
+
+INSERT INTO atividade_projeto(cod_projeto, cod_atividade) VALUES
+(1, 3),
+(2, 5),
+(3, 1),
+(4, 2),
+(5, 4)
